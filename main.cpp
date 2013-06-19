2d1b91aab4313e092191f17177c42f85dfeb83c9
Luca Ottaviano <lottaviano@develer.com>
2013-06-19 12:12:09 +0200

    Load default fonts in application.
    Signed-off-by: Luca Ottaviano <lottaviano@develer.com>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 #include <QtGui/QGuiApplication>
 #include "qtquick2applicationviewer.h"
 
+#include <QFontDatabase>
+#include <QDebug>
+
+void loadFonts()
+{
+    QFontDatabase database;
+    bool installed = false;
+    foreach (const QString &s, QStringList() << "" << "_Bd")
+    {
+        QString path = QCoreApplication::applicationDirPath() + "/resources/fonts/VodafoneRg" + s + ".ttf";
+        int id = database.addApplicationFont(path);
+        if (id == -1) {
+            qWarning() << "Error loading the font:" << path;
+            continue;
+        }
+
+        QStringList families = QFontDatabase::applicationFontFamilies(id);
+        if (families.length() > 0 && !installed) {
+            qDebug() << "Set default font" << path;
+            installed = true;
+            QGuiApplication::setFont(QFont(families.at(0)));
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    loadFonts();
 
     QtQuick2ApplicationViewer viewer;
     viewer.setMainQmlFile(QStringLiteral("qml/demo-qml/main.qml"));
